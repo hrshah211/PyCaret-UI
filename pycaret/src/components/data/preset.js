167b3e1cc4804b9223b26a4bce1b3b1c572d6864
Harsh Shah <hrshah211@gmail.com>
2022-06-06 14:27:16 -0400

    Load Dropdown

--- pycaret/src/components/data/preset.js
+++ pycaret/src/components/data/preset.js
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Preset = () => {
   const [dataFiles, setDataFiles] = useState([]);
+  const [selectedDataset, setSelectedDataset] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedDataset(event.target.value);
+  };
 
   useEffect(() => {
-    fetch("/members").then((res) =>
+    fetch("/datasets").then((res) =>
       res.json().then((data) => {
-        setDataFiles(data.members);
+        setDataFiles(data.files);
       })
     );
   }, []);
   return (
     <>
-      <div>Preset</div>
-      <ul>
-        {dataFiles.map((data) => {
-          return <li key={data}>{data}</li>;
-        })}
-      </ul>
+      <Box>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Datasets</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedDataset}
+            label="Datasets"
+            onChange={handleChange}
+          >
+            {dataFiles.map((data) => {
+              return <MenuItem value={data}>{data}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </Box>
     </>
   );
 };
--- pycaret_server/server.py
+++ pycaret_server/server.py
@@ -1,22 +1,17 @@
 from flask import Flask
 from github import Github
+import pandas as pd
 
-g = Github("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")
+g = Github("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")
 repo = g.get_repo("pycaret/pycaret")
-contents = repo.get_contents('/datasets')
-files = []
-for i in contents:
-  fileName = i.path.split('/')[1]
-  fileName = fileName.replace(' ','%20')
-  raw_url = 'https://raw.githubusercontent.com/hrshah211/Face_And_Mask_Detector/main/DataSet/'+fileName
-  files.append(raw_url)
+index = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/index.csv')
 
 app = Flask(__name__)
 
 
-@app.route("/members")
-def members():
-    return {"members": files}
+@app.route("/datasets")
+def datasets():
+    return {"files": index['Dataset'].tolist()}
 
 
 if __name__ == "__main__":
