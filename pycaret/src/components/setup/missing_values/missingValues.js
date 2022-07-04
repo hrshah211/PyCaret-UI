45a502420a774177f6f3a054db40169290d3f362
hrshah211 <hrshah211@gmail.com>
2022-07-04 12:43:27 -0400

    Missing Values Setup

--- pycaret/src/components/setup/missing_values/missingValues.js
+++ pycaret/src/components/setup/missing_values/missingValues.js
@@ -9,25 +9,124 @@ import {
 import setupParameters from "../setupParameters";
 
 const MissingValues = () => {
-  const [imputationType, setImputationType] = useState("Simple");
-  const [numericImputation, setNumericImputation] = useState("Mean");
-  const [categoricalImputation, setCategoricalImputation] =
-    useState("Constant");
-  const [iterativeImputationIterations, setIterativeImputationIterations] =
-    useState(0);
+  const getDefaultValue = (source) => {
+    const defaultData = source.filter(([i, data]) => data.default === true);
+    return defaultData ? defaultData[0][1] : null;
+  };
+
+  const getSelectedValue = (source, value) => {
+    const selectedData = source.filter(([i, data]) => data.name === value);
+    return selectedData ? selectedData[0][1] : null;
+  };
+
+  //#region Imputation Type
+  const [imputationType, setImputationType] = useState(
+    getDefaultValue(
+      Object.entries(setupParameters.missing_values.imputation_type)
+    )
+  );
 
   const handleImputationTypeChange = (event) => {
-    setImputationType(event.target.value);
+    setImputationType(
+      getSelectedValue(
+        Object.entries(setupParameters.missing_values.imputation_type),
+        event.target.value
+      )
+    );
   };
+  //#endregion
+
+  //#region Numeric Imputation
+  const [numericImputation, setNumericImputation] = useState(
+    getDefaultValue(
+      Object.entries(setupParameters.missing_values.numeric_imputation)
+    )
+  );
+
   const handleNumericImputationChange = (event) => {
-    setNumericImputation(event.target.value);
+    setNumericImputation(
+      getSelectedValue(
+        Object.entries(setupParameters.missing_values.numeric_imputation),
+        event.target.value
+      )
+    );
   };
+  //#endregion
+
+  //#region Other Numeric Imputation
+  const [otherNumericImputation, setOtherNumericImputation] = useState();
+
+  const handleOtherNumericImputationsChange = (event) => {
+    setOtherNumericImputation(event.target.value);
+  };
+  //#endregion
+
+  //#region Categorical Imputation
+  const [categoricalImputation, setCategoricalImputation] = useState(
+    getDefaultValue(
+      Object.entries(setupParameters.missing_values.categorical_imputation)
+    )
+  );
+
   const handleCategoricalImputationChange = (event) => {
-    setCategoricalImputation(event.target.value);
+    setCategoricalImputation(
+      getSelectedValue(
+        Object.entries(setupParameters.missing_values.categorical_imputation),
+        event.target.value
+      )
+    );
   };
+  //#endregion
+
+  //#region Other Categorical Imputation
+  const [otherCategoricalImputation, setOtherCategoricalImputation] =
+    useState();
+
+  const handleOtherCategoricalImputationsChange = (event) => {
+    setOtherCategoricalImputation(event.target.value);
+  };
+  //#endregion
+
+  //#region Numeric Iterative Imputer
+  const [numericIterativeImputer, setNumericIterativeImputer] = useState(
+    getDefaultValue(Object.entries(setupParameters.missing_values.regressors))
+  );
+
+  const handleNumericIterativeImputerChange = (event) => {
+    setNumericIterativeImputer(
+      getSelectedValue(
+        Object.entries(setupParameters.missing_values.regressors),
+        event.target.value
+      )
+    );
+  };
+  //#endregion
+
+  //#region iterative Imputation Iterations
+  const [iterativeImputationIterations, setIterativeImputationIterations] =
+    useState(5);
+
   const handleIterativeImputationIterationsChange = (event) => {
     setIterativeImputationIterations(event.target.value);
   };
+  //#endregion
+
+  //#region Categorical Iterative Imputer
+  const [categoricalIterativeImputer, setCategoricalIterativeImputer] =
+    useState(
+      getDefaultValue(Object.entries(setupParameters.missing_values.regressors))
+    );
+
+  const handleCategoricalIterativeImputerChange = (event) => {
+    setCategoricalIterativeImputer(
+      getSelectedValue(
+        Object.entries(setupParameters.missing_values.regressors),
+        event.target.value
+      )
+    );
+  };
+  //#endregion
+
   return (
     <>
       <StyledTypography
@@ -39,11 +138,11 @@ const MissingValues = () => {
         Missing Values
       </StyledTypography>
       <StyledGrid container pb={1}>
-        <StyledGrid item xs={4} pr={1}>
+        <StyledGrid item xs={3} pr={1}>
           <StyledFormControl fullWidth>
             <InputLabel>Imputation Type</InputLabel>
             <Select
-              value={imputationType}
+              value={imputationType.name}
               label="Imputation Type"
               onChange={handleImputationTypeChange}
             >
@@ -57,96 +156,134 @@ const MissingValues = () => {
             </Select>
           </StyledFormControl>
         </StyledGrid>
-        <StyledGrid item xs={4}>
-          <StyledFormControl fullWidth>
-            <InputLabel>Numeric Imputation</InputLabel>
-            <Select
-              value={numericImputation}
-              label="Numeric Imputation"
-              onChange={handleNumericImputationChange}
-            >
-              {setupParameters.missing_values.numeric_imputation.map((data) => {
-                return (
-                  <MenuItem value={data.name} key={data.code}>
-                    {data.name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </StyledFormControl>
-        </StyledGrid>
-        <StyledGrid item xs={4} pl={1}>
-          <StyledFormControl fullWidth>
-            <InputLabel>Categorical Imputation</InputLabel>
-            <Select
-              value={categoricalImputation}
-              label="Categorical Imputation"
-              onChange={handleCategoricalImputationChange}
-            >
-              {setupParameters.missing_values.categorical_imputation.map(
-                (data) => {
-                  return (
-                    <MenuItem value={data.name} key={data.code}>
-                      {data.name}
-                    </MenuItem>
-                  );
-                }
-              )}
-            </Select>
-          </StyledFormControl>
-        </StyledGrid>
       </StyledGrid>
-      <StyledGrid container pb={1}>
-        <StyledGrid item xs={4} pr={1}>
-          <StyledFormControl fullWidth>
-            <InputLabel>Numeric Iterative Imputer</InputLabel>
-            <Select
-              value={imputationType}
-              label="Numeric Iterative Imputer"
-              onChange={handleImputationTypeChange}
-            >
-              {setupParameters.missing_values.imputation_type.map((data) => {
-                return (
-                  <MenuItem value={data.name} key={data.code}>
-                    {data.name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </StyledFormControl>
-        </StyledGrid>
-        <StyledGrid item xs={4}>
-          <StyledFormControl fullWidth>
-            <TextField
-              label="Iterative Imputation Iterations"
-              type="number"
-              value={iterativeImputationIterations}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              onchange={handleIterativeImputationIterationsChange}
-            />
-          </StyledFormControl>
-        </StyledGrid>
-        <StyledGrid item xs={4} pl={1}>
-          <StyledFormControl fullWidth>
-            <InputLabel>Categorical Iterative Imputer</InputLabel>
-            <Select
-              value={imputationType}
-              label="Categorical Iterative Imputer"
-              onChange={handleImputationTypeChange}
-            >
-              {setupParameters.missing_values.imputation_type.map((data) => {
-                return (
-                  <MenuItem value={data.name} key={data.code}>
-                    {data.name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </StyledFormControl>
-        </StyledGrid>
-      </StyledGrid>
+      {imputationType.name === "Simple" && (
+        <>
+          <StyledGrid container pb={1}>
+            <StyledGrid item xs={3} pr={1}>
+              <StyledFormControl fullWidth>
+                <InputLabel>Numeric Imputation</InputLabel>
+                <Select
+                  value={numericImputation.name}
+                  label="Numeric Imputation"
+                  onChange={handleNumericImputationChange}
+                >
+                  {setupParameters.missing_values.numeric_imputation.map(
+                    (data) => {
+                      return (
+                        <MenuItem value={data.name} key={data.code}>
+                          {data.name}
+                        </MenuItem>
+                      );
+                    }
+                  )}
+                </Select>
+              </StyledFormControl>
+            </StyledGrid>
+            {numericImputation.name === "Other" && (
+              <StyledGrid item xs={3} pr={1}>
+                <StyledFormControl fullWidth>
+                  <TextField
+                    type="number"
+                    label="Other Numeric Imputation"
+                    value={otherNumericImputation}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    onChange={handleOtherNumericImputationsChange}
+                  />
+                </StyledFormControl>
+              </StyledGrid>
+            )}
+            <StyledGrid item xs={3} pr={1}>
+              <StyledFormControl fullWidth>
+                <InputLabel>Categorical Imputation</InputLabel>
+                <Select
+                  value={categoricalImputation.name}
+                  label="Categorical Imputation"
+                  onChange={handleCategoricalImputationChange}
+                >
+                  {setupParameters.missing_values.categorical_imputation.map(
+                    (data) => {
+                      return (
+                        <MenuItem value={data.name} key={data.code}>
+                          {data.name}
+                        </MenuItem>
+                      );
+                    }
+                  )}
+                </Select>
+              </StyledFormControl>
+            </StyledGrid>
+            {categoricalImputation.name === "Other" && (
+              <StyledGrid item xs={3} pr={1}>
+                <StyledFormControl fullWidth>
+                  <TextField
+                    label="Other Categorical Imputation"
+                    value={otherCategoricalImputation}
+                    onChange={handleOtherCategoricalImputationsChange}
+                  />
+                </StyledFormControl>
+              </StyledGrid>
+            )}
+          </StyledGrid>
+        </>
+      )}
+      {imputationType.name === "Iterative" && (
+        <>
+          <StyledGrid container pb={1}>
+            <StyledGrid item xs={4} pr={1}>
+              <StyledFormControl fullWidth>
+                <InputLabel>Numeric Iterative Imputer</InputLabel>
+                <Select
+                  value={numericIterativeImputer.name}
+                  label="Numeric Iterative Imputer"
+                  onChange={handleNumericIterativeImputerChange}
+                >
+                  {setupParameters.missing_values.regressors.map((data) => {
+                    return (
+                      <MenuItem value={data.name} key={data.code}>
+                        {data.name}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </StyledFormControl>
+            </StyledGrid>
+            <StyledGrid item xs={4}>
+              <StyledFormControl fullWidth>
+                <TextField
+                  label="Iterative Imputation Iterations"
+                  type="number"
+                  value={iterativeImputationIterations}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  onChange={handleIterativeImputationIterationsChange}
+                />
+              </StyledFormControl>
+            </StyledGrid>
+            <StyledGrid item xs={4} pl={1}>
+              <StyledFormControl fullWidth>
+                <InputLabel>Categorical Iterative Imputer</InputLabel>
+                <Select
+                  value={categoricalIterativeImputer.name}
+                  label="Categorical Iterative Imputer"
+                  onChange={handleCategoricalIterativeImputerChange}
+                >
+                  {setupParameters.missing_values.regressors.map((data) => {
+                    return (
+                      <MenuItem value={data.name} key={data.code}>
+                        {data.name}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </StyledFormControl>
+            </StyledGrid>
+          </StyledGrid>
+        </>
+      )}
     </>
   );
 };
--- pycaret/src/components/setup/setupParameters.js
+++ pycaret/src/components/setup/setupParameters.js
@@ -1,19 +1,166 @@
 const setupParameters = {
   missing_values: {
+    categorical_imputation: [
+      {
+        code: "mode",
+        name: "Mode",
+        default: true
+      },
+      {
+        code: "drop",
+        name: "Drop"
+      },
+      {
+        code: "other",
+        name: "Other"
+      }
+    ],
     imputation_type: [
-      { name: "Simple", code: 1 },
-      { name: "Iterative", code: 2 },
+      {
+        code: "simple",
+        name: "Simple",
+        default: true
+      },
+      {
+        code: "iterative",
+        name: "Iterative"
+      },
+      {
+        code: "none",
+        name: "None"
+      }
     ],
     numeric_imputation: [
-      { name: "Mean", code: 1 },
-      { name: "Median", code: 2 },
-      { name: "Zero", code: 3 },
+      {
+        code: "mean",
+        name: "Mean",
+        default: true
+      },
+      {
+        code: "median",
+        name: "Median"
+      },
+      {
+        code: "mode",
+        name: "Mode"
+      },
+      {
+        code: "drop",
+        name: "Drop"
+      },
+      {
+        code: "knn",
+        name: "KNN"
+      },
+      {
+        code: "other",
+        name: "Other"
+      }
     ],
-    categorical_imputation: [
-      { name: "Constant", code: 1 },
-      { name: "Mode", code: 2 },
-    ],
-  },
+    regressors: [
+      {
+        code: "lr",
+        name: "Linear Regression"
+      },
+      {
+        code: "lasso",
+        name: "Lasso Regression"
+      },
+      {
+        code: "ridge",
+        name: "Ridge Regression"
+      },
+      {
+        code: "en",
+        name: "Elastic Net"
+      },
+      {
+        code: "lar",
+        name: "Least Angle Regression"
+      },
+      {
+        code: "llar",
+        name: "Lasso Least Angle Regression"
+      },
+      {
+        code: "omp",
+        name: "Orthogonal Matching Pursuit"
+      },
+      {
+        code: "br",
+        name: "Bayesian Ridge"
+      },
+      {
+        code: "ard",
+        name: "Automatic Relevance Determination"
+      },
+      {
+        code: "par",
+        name: "Passive Aggressive Regressor"
+      },
+      {
+        code: "ransac",
+        name: "Random Sample Consensus"
+      },
+      {
+        code: "tr",
+        name: "TheilSen Regressor"
+      },
+      {
+        code: "huber",
+        name: "Huber Regressor"
+      },
+      {
+        code: "kr",
+        name: "Kernel Ridge"
+      },
+      {
+        code: "svm",
+        name: "Support Vector Regression"
+      },
+      {
+        code: "knn",
+        name: "K Neighbors Regressor"
+      },
+      {
+        code: "dt",
+        name: "Decision Tree Regressor"
+      },
+      {
+        code: "rf",
+        name: "Random Forest Regressor"
+      },
+      {
+        code: "et",
+        name: "Extra Trees Regressor"
+      },
+      {
+        code: "ada",
+        name: "AdaBoost Regressor"
+      },
+      {
+        code: "gbr",
+        name: "Gradient Boosting Regressor"
+      },
+      {
+        code: "mlp",
+        name: "MLP Regressor"
+      },
+      {
+        code: "xgboost",
+        name: "Extreme Gradient Boosting"
+      },
+      {
+        code: "lightgbm",
+        name: "Light Gradient Boosting Machine",
+        default: true
+      },
+      {
+        code: "catboost",
+        name: "CatBoost Regressor"
+      }
+    ]
+  }
 };
 
 export default setupParameters;
